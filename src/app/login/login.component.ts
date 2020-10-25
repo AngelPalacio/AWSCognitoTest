6e95ca334745817ba0a2285441661ad3c525c5ff
Angel Palacio <angel.palacio.gonzalez@gmail.com>
2020-10-25 15:05:36 +0100

    Add login form validation

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Auth } from 'aws-amplify';
 
 @Component({
@@ -7,21 +9,51 @@ import { Auth } from 'aws-amplify';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-  constructor() {}
+  form: FormGroup;
+  loading = false;
+  submitted = false;
+  errorMessage = '';
 
-  ngOnInit(): void {}
+  constructor(private formBuilder: FormBuilder, private router: Router) {}
 
-  login() {
-    Auth.signIn('mike', 'passmike')
+  ngOnInit() {
+    this.form = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  get f() {
+    return this.form.controls;
+  }
+
+  onSubmit() {
+    this.errorMessage = '';
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.form.invalid) {
+      return;
+    }
+
+    this.login(this.f.username.value, this.f.password.value, '/user-details');
+  }
+
+  login(user, password, redirectUrl) {
+    Auth.signIn(user, password)
       .then((user) => {
         console.log('Login correcto!');
         console.log(user);
         Auth.currentAuthenticatedUser().then((u) => {
           console.log('El usuario está autenticado');
           console.log(u);
+          this.router.navigateByUrl(redirectUrl);
         });
       })
-      .catch((err) => console.error(err));
+      .catch((err) => {
+        console.error(err);
+        this.errorMessage = err.message;
+      });
   }
 
   logout() {
